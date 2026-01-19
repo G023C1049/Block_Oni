6cd0dc29edae30f4b44470bb635d166b529172c5
oiru <ibuki1215@docomo.ne.jp>
2026-01-19 14:01:37 +0900

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_unity_widget/flutter_unity_widget.dart';
-//Hello~
 
 // --- ここから追加 ---
 void main() {
@@ -11,6 +10,8 @@ void main() {
 // --- ここまで追加 ---
 
 class BattleScreen extends StatefulWidget {
+  const BattleScreen({super.key});
+
   @override
   _BattleScreenState createState() => _BattleScreenState();
 }
